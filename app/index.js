40dcfbe30ceab2bb76993a22dd0c2bc080758d93
wangyan <promiseid@126.com>
2016-12-22 15:06:08 +0800

    多级

---
+++ app/index.js
@@ -0,0 +1,122 @@
+import fetch from 'isomorphic-fetch';
+import 'babel-polyfill'
+class LINKAGE {
+    constructor(json){
+        this._Sel_=[];
+        this.isUnChaneindex=-1;
+        this.first=false;
+        this.selListAll=this.selListAll.bind(this);
+        this.json=json;
+        for(let i=0;i<json.id.length;i++){
+            this._Sel_.push(document.querySelector(json.id[i]));
+        }
+        this.linkAgeData=[];
+        this.indArr=[0,0,0];
+        this.getAjax(json._ajax_)
+            .then((res)=>{
+                this.linkAgeData=res;
+                this.selListAll()
+                this.change();
+                this.first=true;
+            });
+    }
+
+    selListAll(){
+        let result=this.json._ajax_.resKey.result?this.linkAgeData[this.json._ajax_.resKey.result]:this.linkAgeData;
+        let linkAgeArray=this.json._ajax_.resKey.linkAgeArray;//循环等级及相关数据对应的key值名称
+
+        for(let i=0;i<linkAgeArray.length;i++){
+            if(this.isUnChaneindex<i){
+                console.log(i,this.isUnChaneindex)
+                this.pushOption(result,{
+                    id:linkAgeArray[i]['id'],
+                    name:linkAgeArray[i]['name']
+                },this._Sel_[i]);
+                this._Sel_[i].setAttribute('lev',i);
+            }
+            // console.log(i,result,result[0])
+
+            //下一个集合
+            let oChildKey=linkAgeArray[i+1]?linkAgeArray[i+1]['arrKey']:null;
+            if(oChildKey){
+                //alert(this.indArr )
+                result=Object.assign([],[
+                    ...result[this.indArr[i]][oChildKey]
+                ])
+            }
+        }
+    }
+
+    change(){
+        for(let i=0;i<this._Sel_.length;i++){
+            (function (_this,oSel) {
+                oSel.addEventListener('change', function(){
+                    let id=this.value;
+                    let ind=oSel.getAttribute('lev');
+                    let childInd=oSel.selectedIndex;
+                    _this.indArr[ind]=childInd;
+                    _this.isUnChaneindex=ind;
+                    _this.selListAll();
+                },false);
+            })(this,this._Sel_[i]);
+        }
+    }
+
+    pushOption(list,keys,oSel){
+        console.log('list',list)
+        let str='';
+        for(let i=0;i<list.length;i++){
+            str+=`<option value="${list[i][keys.id]}" >
+                                ${list[i][keys.name]}
+                            </option>`
+        }
+        oSel.innerHTML=str;
+    }
+
+    getAjax(_ajax_){
+        let url=_ajax_.url;
+        if(_ajax_.data){
+            url+='?';
+            let urlArray=[];
+            for(let name in _ajax_.data){
+                urlArray.push(name+'='+_ajax_.data[name]);
+            }
+            url+=urlArray.join('&');
+        }
+        return fetch(url,{
+            method:_ajax_.method
+        })
+            .then(response => response.json())
+            .then(json =>{
+                return json;
+            })
+    }
+}
+
+let linkAge=new LINKAGE({
+    id:['#sel1','#sel2','#sel3'],
+    _ajax_:{
+        url:'./test.json',
+        data:{
+            test:'a',
+            t:1
+        },
+        method: "get",
+        resKey:{
+            result:'result',//返回数据的key字段数组,如果没有直接就是返回数据
+            linkAgeArray:[{
+                id:'id',
+                name:'name',
+                arrKey:'result'
+            },{
+                id:'child_id',
+                name:'child_name',
+                arrKey:'child'
+            },{
+                id:'childChild_id',
+                name:'childChild_name',
+                arrKey:'childChild'
+            }]
+        }
+    }
+});
